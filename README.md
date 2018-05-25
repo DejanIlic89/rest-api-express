851081b6a716a2dcc760d906786045905d4efd4c
Dejan Ilic <theekey@gmail.com>
2018-05-25 18:59:39 +0200

    mongodb created

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # rest-api-express
+
+### Resource  
+[REST API Tutorial](https://www.youtube.com/watch?v=BRdcRFvuqsE&list=PL4cUxeGkcC9jBcybHMTIia56aV21o2cZ8&index=1)
--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // set up express app
 const app = express();
+
+// connect to mongodb
+mongoose.connect('mongodb://localhost/ninjago');
+mongoose.Promise = global.Promise;  // because mongoose.Promise is deprecated, so we overriding this with global.Promise
 
 app.use(bodyParser.json());
 
 // initialize routes
 app.use('/api', require('./routes/api'));
+
+// error handling middleware
+app.use(function(err, req, res, next) {
+    // console.log(err);
+    res.status(422).send({error: err.message});
+}); 
 
 // listen for requests
 app.listen(process.env.port || 4000, function() {
